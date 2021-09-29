dd56aa9417070af64ff53087456c4fe8e273db6c
Artyom Tarakanov <atarakanov@gmail.com>
2021-09-30 02:10:28 +0300

    - Add some new pages - Add one new model "wishlist"

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -14,6 +15,23 @@ class Product(models.Model):
     link = models.URLField()
     price = models.IntegerField()
     create_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
+
+
+class WishList(models.Model):
+    """ Таблица "Лист подарков"
+
+    - id
+    - Владелец
+    - product
+    - Видимость (bool)
+    """
+    title = models.CharField(max_length=120)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    is_hidden = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import WishList
 
 def index(request):
     return render(request, 'index.html', {})
 
 
 def about(request):
-    return render(request, 'about.html', {})
+    return render(request, 'about.html', {"title": "Wishlist | about project"})
+
+def list_page(request, pk):
+    wish_list = get_object_or_404(WishList, pk=pk)
+    return render(
+        request,
+        'wish_list.html',
+        {
+            'list': wish_list,
+            'is_owner_list': wish_list.owner == request.user
+        }
+    )
