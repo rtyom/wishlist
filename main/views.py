8220f72c7c5b2bba9926cdab8c1d3c8dd70e1714
Artyom Tarakanov <atarakanov@gmail.com>
2021-09-30 15:37:45 +0300

    - Add some new pages - Add one new model "wishlist"

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import WishList
+from .forms import ProductForm
 
 def index(request):
     return render(request, 'index.html', {})
@@ -11,11 +12,14 @@ def about(request):
 
 def list_page(request, pk):
     wish_list = get_object_or_404(WishList, pk=pk)
-    return render(
-        request,
-        'wish_list.html',
-        {
-            'list': wish_list,
-            'is_owner_list': wish_list.owner == request.user
-        }
+    if request.method.POST:
+        form = ProductForm
+        return render(
+            request,
+            'wish_list.html',
+            {
+                'list': wish_list,
+                'is_owner_list': wish_list.owner == request.user,
+                'form': form
+            }
     )
